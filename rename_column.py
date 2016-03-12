af65780ccf0656181b48f900b283ac4a60be4e14
ye-zhi <yezhi@berkeley.edu>
2016-03-11 22:21:35 -0800

    fix rename

--- rename_column.py
+++ rename_column.py
@@ -4,5 +4,7 @@ import numpy as np
 data = pd.read_csv('AAPL_05222012_0930_1300_LOB_2.csv')
 data.shape
 new_cols = [list(data.columns.values)[i][16:][:-19] for i in range(2,62)]
-data.columns.values[2:] = new_cols
+temp = ['Index','Time']
+temp.extend(new_cols)
+data.columns = temp
 list(data.columns.values)
