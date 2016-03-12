72993f356a543a46df05bcf3709ebe004287e275
lizhua <lizhua@berkeley.edu>
2016-03-12 00:16:57 -0800

    lizhua rename and v3

--- rename_column.py
+++ rename_column.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('AAPL_05222012_0930_1300_LOB_2.csv')
-data.shape
-new_cols = [list(data.columns.values)[i][16:][:-19] for i in range(2,62)]
-temp = ['Index','Time']
-temp.extend(new_cols)
-data.columns = temp
-list(data.columns.values)
+def rename_data():
+	data = pd.read_csv('LOB.csv')
+	new_cols = [list(data.columns.values)[i][16:][:-19] for i in range(2,62)]
+	temp = ['Index','Time']
+	temp.extend(new_cols)
+	data.columns = temp
+	return (data)
+
