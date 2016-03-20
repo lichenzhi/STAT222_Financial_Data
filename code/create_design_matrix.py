e11d5cb28596a81dd09f9ea72bc053e0679753d2
lizhua <lizhua@berkeley.edu>
2016-03-19 18:56:49 -0700

    lizhua add an argument and default value for our v6

--- code/create_design_matrix.py
+++ code/create_design_matrix.py
@@ -5,16 +5,19 @@ from get_v2_v5_function import *
 from get_v3_function import *
 from get_v6_function import * 
 
-def create_design_matrix():
+# create design matrix
+# argument is for v6, derivative
+# default is 30 rows.
+def create_design_matrix(NUM_OF_TIME_STAMP = 30):
     v1_v4 = createV1V4()
     v2_v5 = createV2V5()
     v3 = createV3()
-    v6 = createV6(30)
+    v6 = createV6(NUM_OF_TIME_STAMP)
     design_matrix= pd.concat([v1_v4,v2_v5,v3,v6], axis = 1)
     return (design_matrix)
 
 #if you want to check whether you can run the design matrix.
 #uncomment the following three lines
-test = create_design_matrix()
-print (test.loc[1,:])
-print (test.shape)
+#test = create_design_matrix()
+#print (test.loc[1,:])
+#print (test.shape)
