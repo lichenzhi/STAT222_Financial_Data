9f817d063e42ec5ce227554d8fcaa23377435060
lizhua <lizhua@berkeley.edu>
2016-03-23 18:01:43 -0700

    edit recall matrix

--- code/model.py
+++ code/model.py
@@ -9,8 +9,8 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 
 nrow=20000
-NUM_OF_TIME_STAMP_FOR_DERIV=10
-NUM_OF_TIME_STAMP_FOR_RESPONSE=10
+NUM_OF_TIME_STAMP_FOR_DERIV=30
+NUM_OF_TIME_STAMP_FOR_RESPONSE=20
 
 #get the data for sampling 
 sample_data = sample_by_movement_population(nrow,NUM_OF_TIME_STAMP_FOR_DERIV, 
@@ -45,8 +45,10 @@ testing_data_y.index = range(testing_data_y.shape[0])
 #create svm model 
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
-Cs = np.logspace(-6, -1, 10)
-Gammas = np.logspace(-9, 3, 13)
+#Cs = np.logspace(-6, -1, 10)
+Cs = [0.1]
+#Gammas = np.logspace(-9, 3, 13)
+Gammas = [0]
 svc = svm.SVC()
 clf = GridSearchCV(estimator=svc, param_grid=dict(C=Cs,gamma=Gammas),
                     n_jobs=-1)
@@ -56,41 +58,54 @@ clf = clf.best_estimator_
 clf.fit(X, y)
 prediction = clf.predict(scaler.fit_transform(testing_data))
 prediction = pd.DataFrame(prediction)
-#recall to the whole training data
-recall = clf.predict(X)
-recall = pd.DataFrame(recall)
-#combine the prediction, reacall and true value 
+#fit to the whole training data
+train = clf.predict(X)
+train = pd.DataFrame(train)
+#combine the prediction, train and true value 
 predict_true = pd.concat([prediction,testing_data_y],axis=1)
-recall_true = pd.concat([recall,pd.DataFrame(y)],axis=1)
+train_true = pd.concat([train,pd.DataFrame(y)],axis=1)
 #name the columns 
 predict_true.columns = ['predict_mid_price_movement','true_mid_price_movement']
-recall_true.columns = ['recall_mid_price_movement','true_mid_price_movement']
-#total number of testing data and recall data
+train_true.columns = ['train_mid_price_movement','true_mid_price_movement']
+#total number of testing data and train data
 predict_total = predict_true.shape[0]
-recall_total = recall_true.shape[0]
+train_total = train_true.shape[0]
 #initialize the value 
 predict_correct = 0 
-recall_correct = 0
-#check how many predictions and recall are correct
+train_correct = 0
+#check how many predictions and train are correct
 for i in range(predict_total):
     if predict_true.iloc[i,0]==predict_true.iloc[i,1]:
         predict_correct = predict_correct + 1
 
-for i in range(recall_total):
-    if recall_true.iloc[i,0]==recall_true.iloc[i,1]:
-        recall_correct = recall_correct + 1 
+for i in range(train_total):
+    if train_true.iloc[i,0]==train_true.iloc[i,1]:
+        train_correct = train_correct + 1 
 
 print ("------------------------------------------------------------")
 print ("Result of SVM")
 print ("------------------------------------------------------------")
-print ("Overall Performace of Prediction")
+print ("Overall Performace of training")
+print (float(train_correct)/train_total)
+print ("Overall Performace of Testing")
 print (float(predict_correct)/predict_total)
-print ("Overall Performace of Recall")
-print (float(recall_correct)/recall_total)
 print ("------------------------------------------------------------")
 
-#prediction accurate matrix 
-print ("Prediction Accurate Count Matrix ")
+#train accurate matrix 
+print ("Training Accurate Rate Matrix ")
+train_accurate_count_matrix = pd.crosstab(index=train_true.iloc[:,0], 
+                           columns=train_true.iloc[:,1],margins=True)    
+train_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
+train_accurate_count_matrix.rename(index ={-1.0:'down',0.0:"stationary",1.0:"up","All":"colTotal"},inplace=True)
+print (train_accurate_count_matrix)
+print (" ")
+print ("train Accurate Rate Matrix ")
+train_accurate_rate_matrix = train_accurate_count_matrix.T / train_accurate_count_matrix["rowTotal"]
+print (train_accurate_rate_matrix.T)
+print ("------------------------------------------------------------")
+
+#Testing accurate matrix 
+print ("Testing Accurate Count Matrix ")
 prediction_accurate_count_matrix = pd.crosstab(index=predict_true.iloc[:,0], 
                            columns=predict_true.iloc[:,1],margins=True)    
 prediction_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
@@ -100,20 +115,14 @@ print (" ")
 print ("Prediction Accurate Rate Matrix ")
 prediction_accurate_rate_matrix = prediction_accurate_count_matrix.T / prediction_accurate_count_matrix["rowTotal"]
 print (prediction_accurate_rate_matrix.T)
-print ("------------------------------------------------------------")
-
-#recall accurate matrix 
-print ("Recall Accurate Rate Matrix ")
-recall_accurate_count_matrix = pd.crosstab(index=recall_true.iloc[:,0], 
-                           columns=recall_true.iloc[:,1],margins=True)    
-recall_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
-recall_accurate_count_matrix.rename(index ={-1.0:'down',0.0:"stationary",1.0:"up","All":"colTotal"},inplace=True)
-print (recall_accurate_count_matrix)
 print (" ")
 print ("Recall Accurate Rate Matrix ")
-recall_accurate_rate_matrix = recall_accurate_count_matrix.T / recall_accurate_count_matrix["rowTotal"]
-print (recall_accurate_rate_matrix.T)
+prediction_accurate_rate_matrix = prediction_accurate_count_matrix / prediction_accurate_count_matrix.ix["colTotal"]
+print (prediction_accurate_rate_matrix)
 print ("------------------------------------------------------------")
+
+
+
 print ("END SVM")
 print ("------------------------------------------------------------")
 print (" ")
@@ -127,7 +136,7 @@ print (" ")
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 es = [100]
-fs = [2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+fs = [11]
 rf = RandomForestClassifier()
 clf = GridSearchCV(estimator=rf, param_grid=dict(n_estimators=es,max_features=fs),
                     n_jobs=-1)
@@ -138,41 +147,55 @@ clf.fit(X, y)
 #predict the testing data and convert to data frame 
 prediction = clf.predict(scaler.fit_transform((testing_data)))
 prediction = pd.DataFrame(prediction)
-#recall to the whole training data
-recall = clf.predict(X)
-recall = pd.DataFrame(recall)
-#combine the prediction, reacall and true value 
+
+#fit to the whole training data
+train = clf.predict(X)
+train = pd.DataFrame(train)
+#combine the prediction, train and true value 
 predict_true = pd.concat([prediction,testing_data_y],axis=1)
-recall_true = pd.concat([recall,pd.DataFrame(y)],axis=1)
+train_true = pd.concat([train,pd.DataFrame(y)],axis=1)
 #name the columns 
 predict_true.columns = ['predict_mid_price_movement','true_mid_price_movement']
-recall_true.columns = ['recall_mid_price_movement','true_mid_price_movement']
-#total number of testing data and recall data
+train_true.columns = ['train_mid_price_movement','true_mid_price_movement']
+#total number of testing data and train data
 predict_total = predict_true.shape[0]
-recall_total = recall_true.shape[0]
+train_total = train_true.shape[0]
 #initialize the value 
 predict_correct = 0 
-recall_correct = 0
-#check how many predictions and recall are correct
+train_correct = 0
+#check how many predictions and train are correct
 for i in range(predict_total):
     if predict_true.iloc[i,0]==predict_true.iloc[i,1]:
         predict_correct = predict_correct + 1
 
-for i in range(recall_total):
-    if recall_true.iloc[i,0]==recall_true.iloc[i,1]:
-        recall_correct = recall_correct + 1 
+for i in range(train_total):
+    if train_true.iloc[i,0]==train_true.iloc[i,1]:
+        train_correct = train_correct + 1 
 
 print ("------------------------------------------------------------")
-print ("Result of RF")
+print ("Result of RandomForestClassifier")
 print ("------------------------------------------------------------")
-print ("Overall Performace of Prediction")
+print ("Overall Performace of training")
+print (float(train_correct)/train_total)
+print ("Overall Performace of Testing")
 print (float(predict_correct)/predict_total)
-print ("Overall Performace of Recall")
-print (float(recall_correct)/recall_total)
 print ("------------------------------------------------------------")
 
-#prediction accurate matrix 
-print ("Prediction Accurate Count Matrix ")
+#train accurate matrix 
+print ("Training Accurate Rate Matrix ")
+train_accurate_count_matrix = pd.crosstab(index=train_true.iloc[:,0], 
+                           columns=train_true.iloc[:,1],margins=True)    
+train_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
+train_accurate_count_matrix.rename(index ={-1.0:'down',0.0:"stationary",1.0:"up","All":"colTotal"},inplace=True)
+print (train_accurate_count_matrix)
+print (" ")
+print ("train Accurate Rate Matrix ")
+train_accurate_rate_matrix = train_accurate_count_matrix.T / train_accurate_count_matrix["rowTotal"]
+print (train_accurate_rate_matrix.T)
+print ("------------------------------------------------------------")
+
+#Testing accurate matrix 
+print ("Testing Accurate Count Matrix ")
 prediction_accurate_count_matrix = pd.crosstab(index=predict_true.iloc[:,0], 
                            columns=predict_true.iloc[:,1],margins=True)    
 prediction_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
@@ -182,20 +205,12 @@ print (" ")
 print ("Prediction Accurate Rate Matrix ")
 prediction_accurate_rate_matrix = prediction_accurate_count_matrix.T / prediction_accurate_count_matrix["rowTotal"]
 print (prediction_accurate_rate_matrix.T)
-print ("------------------------------------------------------------")
-
-#recall accurate matrix 
-print ("Recall Accurate Rate Matrix ")
-recall_accurate_count_matrix = pd.crosstab(index=recall_true.iloc[:,0], 
-                           columns=recall_true.iloc[:,1],margins=True)    
-recall_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
-recall_accurate_count_matrix.rename(index ={-1.0:'down',0.0:"stationary",1.0:"up","All":"colTotal"},inplace=True)
-print (recall_accurate_count_matrix)
 print (" ")
 print ("Recall Accurate Rate Matrix ")
-recall_accurate_rate_matrix = recall_accurate_count_matrix.T / recall_accurate_count_matrix["rowTotal"]
-print (recall_accurate_rate_matrix.T)
+prediction_accurate_rate_matrix = prediction_accurate_count_matrix / prediction_accurate_count_matrix.ix["colTotal"]
+print (prediction_accurate_rate_matrix)
 print ("------------------------------------------------------------")
+
 print ("END RF")
 print ("------------------------------------------------------------")
 print (" ")
@@ -209,10 +224,11 @@ print (" ")
 #create rf model
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
-es = 100
-ls = np.linspace(0.0001, 1, 10)
+es = [100]
+#ls = np.linspace(0.0001, 1, 10)
+ls = [0.5]
 gb = GradientBoostingClassifier()
-clf = GridSearchCV(estimator=rf, param_grid=dict(n_estimators=es,learning_rate=ls),
+clf = GridSearchCV(estimator=gb, param_grid=dict(n_estimators=es,learning_rate=ls),
                     n_jobs=-1)
 clf.fit(X, y)                               
 clf = clf.best_estimator_ 
@@ -221,41 +237,54 @@ clf.fit(X, y)
 #predict the testing data and convert to data frame 
 prediction = clf.predict(scaler.fit_transform((testing_data)))
 prediction = pd.DataFrame(prediction)
-#recall to the whole training data
-recall = clf.predict(X)
-recall = pd.DataFrame(recall)
-#combine the prediction, reacall and true value 
+#fit to the whole training data
+train = clf.predict(X)
+train = pd.DataFrame(train)
+#combine the prediction, train and true value 
 predict_true = pd.concat([prediction,testing_data_y],axis=1)
-recall_true = pd.concat([recall,pd.DataFrame(y)],axis=1)
+train_true = pd.concat([train,pd.DataFrame(y)],axis=1)
 #name the columns 
 predict_true.columns = ['predict_mid_price_movement','true_mid_price_movement']
-recall_true.columns = ['recall_mid_price_movement','true_mid_price_movement']
-#total number of testing data and recall data
+train_true.columns = ['train_mid_price_movement','true_mid_price_movement']
+#total number of testing data and train data
 predict_total = predict_true.shape[0]
-recall_total = recall_true.shape[0]
+train_total = train_true.shape[0]
 #initialize the value 
 predict_correct = 0 
-recall_correct = 0
-#check how many predictions and recall are correct
+train_correct = 0
+#check how many predictions and train are correct
 for i in range(predict_total):
     if predict_true.iloc[i,0]==predict_true.iloc[i,1]:
         predict_correct = predict_correct + 1
 
-for i in range(recall_total):
-    if recall_true.iloc[i,0]==recall_true.iloc[i,1]:
-        recall_correct = recall_correct + 1 
+for i in range(train_total):
+    if train_true.iloc[i,0]==train_true.iloc[i,1]:
+        train_correct = train_correct + 1 
 
 print ("------------------------------------------------------------")
 print ("Result of GradientBoostingClassifier")
 print ("------------------------------------------------------------")
-print ("Overall Performace of Prediction")
+print ("Overall Performace of training")
+print (float(train_correct)/train_total)
+print ("Overall Performace of Testing")
 print (float(predict_correct)/predict_total)
-print ("Overall Performace of Recall")
-print (float(recall_correct)/recall_total)
 print ("------------------------------------------------------------")
 
-#prediction accurate matrix 
-print ("Prediction Accurate Count Matrix ")
+#train accurate matrix 
+print ("Training Accurate Rate Matrix ")
+train_accurate_count_matrix = pd.crosstab(index=train_true.iloc[:,0], 
+                           columns=train_true.iloc[:,1],margins=True)    
+train_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
+train_accurate_count_matrix.rename(index ={-1.0:'down',0.0:"stationary",1.0:"up","All":"colTotal"},inplace=True)
+print (train_accurate_count_matrix)
+print (" ")
+print ("train Accurate Rate Matrix ")
+train_accurate_rate_matrix = train_accurate_count_matrix.T / train_accurate_count_matrix["rowTotal"]
+print (train_accurate_rate_matrix.T)
+print ("------------------------------------------------------------")
+
+#Testing accurate matrix 
+print ("Testing Accurate Count Matrix ")
 prediction_accurate_count_matrix = pd.crosstab(index=predict_true.iloc[:,0], 
                            columns=predict_true.iloc[:,1],margins=True)    
 prediction_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
@@ -265,20 +294,12 @@ print (" ")
 print ("Prediction Accurate Rate Matrix ")
 prediction_accurate_rate_matrix = prediction_accurate_count_matrix.T / prediction_accurate_count_matrix["rowTotal"]
 print (prediction_accurate_rate_matrix.T)
-print ("------------------------------------------------------------")
-
-#recall accurate matrix 
-print ("Recall Accurate Rate Matrix ")
-recall_accurate_count_matrix = pd.crosstab(index=recall_true.iloc[:,0], 
-                           columns=recall_true.iloc[:,1],margins=True)    
-recall_accurate_count_matrix.rename(columns = {-1.0:'down',0.0:"stationary",1.0:"up","All":"rowTotal"},inplace=True)
-recall_accurate_count_matrix.rename(index ={-1.0:'down',0.0:"stationary",1.0:"up","All":"colTotal"},inplace=True)
-print (recall_accurate_count_matrix)
 print (" ")
 print ("Recall Accurate Rate Matrix ")
-recall_accurate_rate_matrix = recall_accurate_count_matrix.T / recall_accurate_count_matrix["rowTotal"]
-print (recall_accurate_rate_matrix.T)
+prediction_accurate_rate_matrix = prediction_accurate_count_matrix / prediction_accurate_count_matrix.ix["colTotal"]
+print (prediction_accurate_rate_matrix)
 print ("------------------------------------------------------------")
+
 print ("END GradientBoostingClassifier")
 print ("------------------------------------------------------------")
 
